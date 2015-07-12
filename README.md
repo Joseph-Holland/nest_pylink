29f0d6b47fdf113a08632ac19c68536e2baaf086
Joseph Holland <info@josephholland.ie>
2015-07-12 01:28:43 +0100

    Updated nest_pylink.py with error handling and updated README.md.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ This is a simple Python script that I put together to help gather date from a Ne
 
 This script queries the Nest API and pulls back both ambient temperature and humidity and then uses the Emoncms API to input these values into feeds.
 
+By the way to code for inputting feeds was based off of the emoncms pylink script here: https://github.com/emoncms/development/blob/master/Tutorials/Python/PyLink/pylink.py
+
 The Python may not be that great as this is my first foray into it, so I do apologise...  I'll update this over time if needed.
--- nest_pylink.py
+++ nest_pylink.py
@@ -5,48 +5,61 @@ import httplib
 import requests
 import json
 
-# Define hosts for http calls
-emoncmshost = "localhost"
+# Nest API auth token.
+# See here for details: https://developer.nest.com/documentation/cloud/how-to-auth
+nestauthtoken = "<nest_auth_token>"
 
-# Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
+# Nest device ID.
+nestdeviceid = "<nest_device_id>"
+
+# Host you want to post to: localhost would be an
+# emoncms installation on this computer, this could
+# be changed to emoncms.org to post to emoncms.org.
+emoncmshost = "<host>"
+
+# Location of emoncms install on your server, the
+# standard setup is to place it in a folder called emoncms
 # To post to emoncms.org change this to blank: ""
 emoncmspath = "emoncms"
 
-# Write apikey of emoncms account
+# Write apikey of emoncms account.
 emoncmsapikey = "<emoncms_api_key>"
 
-# Node id youd like the emontx to appear as
+# Node id youd like the emontx to appear from.
 nodeid = 1
 
+# Define HTTP connection to emoncms host.
 emoncmsconn = httplib.HTTPConnection(emoncmshost)
 
-# Read in json from Nest API - Your Nest API token goes here after devices?auth=
-r = requests.get('https://developer-api.nest.com/devices?auth=<nest_token>')
-data = r.json()
+try:
+  # Read in json from Nest API - Your Nest API token goes here after devices?auth=
+  r = requests.get('https://developer-api.nest.com/devices?auth='+nestauthtoken)
+except Exception, msg:
+  print "Error contacting Nest API."
+  print msg
+else:
+  data = r.json()
+  # Print data to console in JSON format.
+  print json.dumps(data, sort_keys=False, indent=4)
+  # Assign temperature and humidity values to new variables.
+  temp = data['thermostats'][nestdeviceid]['ambient_temperature_c']
+  humidity = data['thermostats'][nestdeviceid]['humidity']
 
-#try:
-#  decoded json.loads(data)
-
-print json.dumps(data, sort_keys=False, indent=4)
-print "Ambient temperature is: ", data['thermostats']['<nest_device_id>']['ambient_temperature_c']
-temp = data['thermostats']['<nest_device_id>']['ambient_temperature_c']
-humidity = data['thermostats']['<nest_device_id>']['humidity']
-
-#except (ValueError, KeyError, TypeError):
-#  print "JSON format error"
-
-# Send to emoncms
-#try
-print "Send data to EmonCMS"
-# Send temp
-emoncmsconn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+emoncmsapikey+"&node="+str(nodeid)+"&json={AmbientTempC:"+str(temp)+"}")
-response = emoncmsconn.getresponse()
-print "Response"
-print response.status, response.reason
-emoncmsconn.close()
-# Send humidity
-emoncmsconn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+emoncmsapikey+"&node="+str(nodeid)+"&json={Humidity:"+str(humidity)+"}")
-response = emoncmsconn.getresponse()
-print "Response"
-print response.status, response.reason
-emoncmsconn.close()
+  try:
+    # Send to emoncms.  
+    print "Send data to EmonCMS"
+    # Send temp value to feed.
+    emoncmsconn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+emoncmsapikey+"&node="+str(nodeid)+"&json={AmbientTempC:"+str(temp)+"}")
+    response = emoncmsconn.getresponse()
+    print "Response"
+    print response.status, response.reason
+    emoncmsconn.close()
+    # Send humidity value to feed.
+    emoncmsconn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+emoncmsapikey+"&node="+str(nodeid)+"&json={Humidity:"+str(humidity)+"}")
+    response = emoncmsconn.getresponse()
+    print "Response"
+    print response.status, response.reason
+    emoncmsconn.close()
+  except Exception, msg:
+    print "Error sending to emoncms."
+    print msg
